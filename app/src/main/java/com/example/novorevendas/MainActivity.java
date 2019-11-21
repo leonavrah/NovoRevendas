cc03cbabaa5822f2cf89aa40173e568eb8199709
william <william.teixeira@sempreceub.com>
2019-11-20 21:26:12 -0300

    revendas+ novo

---
+++ app/src/main/java/com/example/novorevendas/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.novorevendas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnEntrar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnEntrar = findViewById(R.id.cmdentrar);
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getApplicationContext(),Menu.class);
+                startActivity(intent);
+
+
+
+
+            }
+        });
+    }
+}
